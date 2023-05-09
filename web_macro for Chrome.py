8e9cfd348190b0187f6333c9f8dbb801804a8713
Luca388 <dkdls388@gmail.com>
2023-05-09 16:18:24 +0900

    firstcommit

---
+++ web_macro for Chrome.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+driver = webdriver.Chrome('/Users/yu/Desktop/Coding/Python_Web_Macro for Chrome')
+
+url ='https://www.google.com/'
+
+driver.get(url)
+
+
+element = driver.find_element(By.CLASS_NAME, 'gLFyf')
+time.sleep(10)
+driver.quit()
