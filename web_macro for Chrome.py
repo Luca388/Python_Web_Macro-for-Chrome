9d0b1b3805ac7b26d5a480ff63113f8105f31634
Luca388 <dkdls388@gmail.com>
2023-05-09 16:44:02 +0900

    add enter

--- web_macro for Chrome.py
+++ web_macro for Chrome.py
@@ -3,6 +3,8 @@ import requests
 from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 driver = webdriver.Chrome('/Users/yu/Desktop/Coding/Python_Web_Macro for Chrome')
 
 url ='https://www.google.com/'
@@ -11,8 +13,11 @@ driver.get(url)
 
 
 element = driver.find_element(By.CLASS_NAME, 'gLFyf')
+
+element.send_keys('test')
+element.send_keys(Keys.ENTER)
+#element.click()
+
+# time sleep and quit
 time.sleep(10)
 driver.quit()
-
-
-#
